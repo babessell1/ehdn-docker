095d6188b6043821a92aded2cc4774844a345a94
babessell1 <babessell@gmail.com>
2023-08-18 16:50:49 -0400

    [fix] full path to ehdn

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# Use Ubuntu 22.04 as the base image
+# add
+FROM ubuntu:22.04
+
+# Install required dependencies
+RUN apt-get update && apt-get install -y \
+    curl \
+    wget \
+    unzip \
+    tar \
+    git \
+    build-essential \
+    samtools \
+    parallel
+
+WORKDIR /usr/local/bin
+
+RUN wget https://github.com/Illumina/ExpansionHunterDenovo/releases/download/v0.9.0/ExpansionHunterDenovo-v0.9.0-linux_x86_64.tar.gz
+
+RUN tar -xvzf ExpansionHunterDenovo-v0.9.0-linux_x86_64.tar.gz
+
+# Copy your application code into the container
+COPY run_ehdn.sh .
+RUN chmod +x run_ehdn.sh
+
+# give me all the permissions
+RUN chmod -R 777 /var/lib/ 
+
+# Set the entrypoint command
+CMD ["run_ehdn.sh"]
---
+++ run_ehdn.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+extract_subject_name() {
+    # get sample name from cram/bam filename
+    local string=$1
+    local pattern="([A-Za-z-]+[A-Za-z0-9-]+-[A-Za-z-]+-[A-Za-z0-9]+)"
+    local match=""
+
+    if [[ $string =~ $pattern ]]; then
+        match="${BASH_REMATCH[1]}"
+    fi
+
+    echo "$match"
+}
+
+process_file() {
+    local cram="$1"
+    local fasta="$2"
+
+    local fname=$(basename "$fasta")
+    local bname=$(basename "$cram" .cram)
+
+    # read only input directories cram/bam, index
+    local ro_cram_dir=$(dirname "$cram")
+    local ro_cramidx_dir=$(dirname "$cramidx")
+    
+    # strling requires idx in same folder
+    #cp "${ro_cramidx_dir}/${bname}.cram.crai" "${ro_cram_dir}/${bname}.cram.crai"
+    samtools index -@ 2 "$cram"
+
+    # run expansionhunter denovo
+    ExpansionHunterDenovo-v0.9.0-linux_x86_64/bin/ExpansionHunterDenovo profile \
+        --reads "$cram" \
+        --reference "$fasta" \
+        --output-prefix output/"$bname" \
+        --min-anchor-mapq 50 \
+        --max-irr-mapq 40
+}
+
+# command-line (cwl file) arguments
+cram1="$1"
+cram2="$2"
+fasta="$3"
+fastaidx="$4"
+
+ro_fa_dir=$(dirname "$fasta")
+ro_faidx_dir=$(dirname "$fastaidx")
+bname_fa=$(basename "$fasta" .fa)
+cp "${ro_faidx_dir}/${bname_fa}.fa.fai" "${ro_fa_dir}/${bname_fa}.fa.fai"
+
+# out = dir to export to
+mkdir -p output
+mkdir -p out
+
+# Run the script in parallel for both cram1 and cram2
+(
+    process_file "$cram1" "$fasta"
+) &
+(
+    process_file "$cram2" "$fasta"
+) &
+
+# Wait for all parallel processes to finish
+wait
+name1=$(extract_subject_name "$(basename "$cram1" .cram)")
+name2=$(extract_subject_name "$(basename "$cram2" .cram)")
+
+tar cf ${name1}___${name2}.tar output
+mv ${name1}___${name2}.tar out/${name1}___${name2}.tar
