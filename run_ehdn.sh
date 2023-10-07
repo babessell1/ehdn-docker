0202664430c6cc7d820d79354ccc19087edc679f
babessell1 <babessell@gmail.com>
2023-10-07 15:32:30 -0400

    [change] better failure handling

--- run_ehdn.sh
+++ run_ehdn.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-extract_subject_name() {
-    # get sample name from cram/bam filename
-    local string="$1"
-    local delimiter="_vcpa"
-    local subject_name=""
-
-    # Use the delimiter "_vcpa" to split the string and extract the subject name
-    subject_name="${string%%$delimiter*}"
-
-    echo "$subject_name"
+# Function to clean up files associated with a CRAM
+cleanup() {
+    local cram="$1"
+    local bname=$(basename "$cram" .cram)
+    
+    # Remove CRAM files and associated output
+    rm -f "output/${bname}*"
 }
 
 process_file() {
@@ -21,10 +18,7 @@ process_file() {
 
     # read only input directories cram/bam, index
     local ro_cram_dir=$(dirname "$cram")
-    local ro_cramidx_dir=$(dirname "$cramidx")
-    
-    # strling requires idx in same folder
-    #cp "${ro_cramidx_dir}/${bname}.cram.crai" "${ro_cram_dir}/${bname}.cram.crai"
+
     samtools index -@ 2 "$cram"
 
     # run expansionhunter denovo
@@ -36,7 +30,16 @@ process_file() {
         --max-irr-mapq 40
 
     # python ehdn_lrdn.py case_sample1.bam case_sample1 ehdn_output.tsv case_sample1.str_profile.json lrdn_output_dir/
-    python3 ehdn_lrdn.py "$cram" "$bname" output/"$bname".tsv output/"$bname".str_profile.json output/
+    python3 /usr/local/bin/ExpansionHunterDenovo-LRDN/ehdn_lrdn.py "$cram" "$bname" output/"$bname".tsv output/"$bname".str_profile.json output/
+
+    # If the process fails, set the failed CRAM variable
+    if [ $? -ne 0 ]; then
+        failed_cram="$cram"
+        # Trigger the cleanup function here to ensure it has a valid failed_cram value
+        cleanup "$failed_cram"
+        # set $? to 1 to indicate that the process failed without exiting the script
+        return 1
+    fi
 }
 
 # command-line (cwl file) arguments
@@ -57,15 +60,48 @@ mkdir -p out
 # Run the script in parallel for both cram1 and cram2
 (
     process_file "$cram1" "$fasta"
+    task1_status=$?  # Store the exit status of task1
 ) &
 (
     process_file "$cram2" "$fasta"
+    task2_status=$?  # Store the exit status of task2
 ) &
+
 
 # Wait for all parallel processes to finish
 wait
+
+
+# Check if either of the tasks were killed (exit code is non-zero)
+if [ "$task1_status" -ne 0 ] || [ "$task2_status" -ne 0 ]; then
+    echo "One or more tasks were killed. Cleaning up..."
+
+    # Determine which task failed and create a TAR for the successful task
+    if [ "$task1_status" -eq 0 ]; then
+        cleanup "$cram2"
+        name1=$(extract_subject_name "$(basename "$cram1" .cram)")
+        echo "Task 1 failed. Creating TAR for $name1 only..."
+        tar cf "out/${name1}.tar" "output"
+        # exit with a pass to ensure tibanna will take what it can get
+        exit 0
+    elif [ "$task2_status" -eq 0 ]; then
+        cleanup "$cram1"
+        name2=$(extract_subject_name "$(basename "$cram2" .cram)")
+        echo "Task 2 failed. Creating TAR for $name2 only..."
+        tar cf "out/${name2}.tar" "output"
+        # exit with a pass to ensure tibanna will take what it can get
+        exit 0
+    else
+        echo "Both tasks failed. No TAR files will be created."
+        # exit with a failure code
+        exit 1
+    fi
+fi
+
 name1=$(extract_subject_name "$(basename "$cram1" .cram)")
 name2=$(extract_subject_name "$(basename "$cram2" .cram)")
 
-tar cf ${name1}___${name2}.tar output
-mv ${name1}___${name2}.tar out/${name1}___${name2}.tar
+echo "${name1}___${name2}.tar"
+
+tar cf "out/${name1}___${name2}.tar" "output"
+
